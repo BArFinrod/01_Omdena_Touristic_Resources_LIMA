0e75d639aea337c9ab193002fc6ae688768f473b
Bar.finrod <erre.bar@gmail.com>
2023-11-26 07:45:13 -0500

    first commit

---
+++ 02_Scripts/Recursos_turisticos.py
@@ -0,0 +1,64 @@
+#%%
+import pandas as pd
+import streamlit as st
+from streamlit_plotly_events import plotly_events
+import plotly.express as px
+from pathlib import Path
+
+
+#%%
+df0 = pd.read_csv(Path(__file__).parent.parent / "01_Data\Inventario_recursos_turisticos.csv", encoding='latin', sep=';')
+dflima = df0.loc[df0["REGIÓN"]=='Lima'].dropna(subset=["LATITUD","LONGITUD"]).rename({'LATITUD':'LONGITUD','LONGITUD':'LATITUD'}, axis=1)
+dflima["CATEGORÍA"] = dflima["CATEGORÍA"].astype('category')
+dflima.index = range(dflima.shape[0])
+
+# %%
+
+st.title("Inventario de recursos turísticos de Lima región")
+st.text("Autor: Ronal Wilfredo Arela Bobadilla")
+
+st.text("Nota: Información elaborada con datos del Ministerio de Comercio Exterior y Turismo")
+
+st.text("Use las herramientas de selección para elegir un grupo de recursos y su información se mostrará en la tabla debajo.")
+
+st.subheader("Figura 1. Mapa de recursos turísticos de Lima región")
+
+band_colors_list =  ['green', 'red', 'blue', 'orange', 'purple', 'gray', 'yellow']
+figm2 = px.scatter_mapbox(dflima, lon="LONGITUD", lat="LATITUD", color="CATEGORÍA",
+                          zoom=11, color_discrete_sequence=band_colors_list,
+                          hover_data=['NOMBRE DEL RECURSO','CATEGORÍA'])
+figm2.update_layout(mapbox_style="carto-positron",
+                    margin=dict(l=20, r=20, t=20, b=20),
+                    mapbox=dict(
+                    bearing=0,
+                    center=dict(
+                        lat=-12.116747,
+                        lon=-77.043542
+                    ),
+                    pitch=0,
+                    zoom=8
+                    ),
+                    legend=dict(
+                    orientation="h",
+                    yanchor="bottom",
+                    y=1.02,
+                    xanchor="right",
+                    x=1
+                )
+            )
+
+selected_points = plotly_events(figm2, click_event=False, select_event=True)
+
+points = []
+for point in selected_points:
+  points.append(dflima.loc[(dflima['CATEGORÍA'].cat.codes==point['curveNumber'])].iloc[[point['pointIndex']]])
+# dfpoints = dflima.loc[dflima['CATEGORÍA'].cat.codes==points].iloc[points]
+# print(points)
+# print(selected_points)
+if len(points)>0:
+    dfpoints = pd.concat(points)
+else:
+   dfpoints = pd.DataFrame()
+
+st.subheader("Tabla 01. Tabla de información")
+st.table(dfpoints)
---
+++ README.md
@@ -0,0 +1,33 @@
+# Inventario de recursos turísticos en Lima región.
+
+## Description
+
+This project georeference the touristic resources of the region of Lima, Perú.
+
+Information had been taken from: https://www.datosabiertos.gob.pe/dataset/inventario-nacional-de-recursos-tur%C3%ADsticos
+
+Touristic resources were grouped by type:
+
+1. Natural places (Sitios Naturales)
+2. Cultural manifestations (Manifestaciones Culturales)
+3. Folklore (Folclore)
+4. Contemporary Technical, Scientific and Artistic Accomplishments (Realizaciones Técnicas, Científicas y Artísticas Contemporáneas)
+5. Scheduled events (Acontecimientos programados)
+
+The App allow the user to interact and select a group of resources in order to gather information about each in a table.
+
+## Charts
+![Alt text](03_Images/Dashboard_img1.png)
+
+## Conclusion
+
+This App shows the inventory of tourism resources in Lima, Perú.
+
+By knowing the exact location of each place and showing in an intereactive App bring us the opportunity to explore and detect groups of resources that can be visit together in order to save time to the tourist.
+
+It also allow us to explore a future mathematical tool that can find the shortest path between selected places in order to save time and take advantage of the trip. It could also show recommendations to the tourist based on preferences and closeness of the place.
+
+
+## References
+
+1. https://www.datosabiertos.gob.pe/dataset/inventario-nacional-de-recursos-tur%C3%ADsticos
